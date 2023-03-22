e7f093d51e620f1edbb3becad9d49ad106290c6d
Olga Mamonova <shamrilo91@mail.ru>
2023-03-22 11:37:21 +0300

    ffa

--- source/js/modules/form-validate/form.js
+++ source/js/modules/form-validate/form.js
@@ -10,32 +10,6 @@ export class Form {
     this._validState = false;
   }
 
-  _resetSelect(select) {
-    const nativeSelect = select.querySelector('select');
-    const activeIndex = nativeSelect.options.selectedIndex;
-    const selectedOption = nativeSelect.options[activeIndex];
-    const buttonText = select.querySelector('.custom-select__text');
-    const selectItems = select.querySelectorAll('.custom-select__item');
-    buttonText.textContent = selectedOption.textContent;
-    selectItems.forEach((item, index) => {
-      if (index === activeIndex - 1) {
-        item.setAttribute('aria-selected', 'true');
-        return;
-      }
-      item.setAttribute('aria-selected', 'false');
-    });
-    if (!nativeSelect.value) {
-      select.classList.remove('not-empty');
-      select.classList.remove('is-valid');
-    }
-  }
-
-  _resetSelects(form) {
-    const selects = form.querySelectorAll('[data-select]');
-    selects.forEach((select) => {
-      this._resetSelect(select);
-    });
-  }
 
   reset(form) {
     this._validator._reset();
